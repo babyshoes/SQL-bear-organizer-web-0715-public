0151bf4bdafd47ccf08111861494a2fe971a9a90
babyshoes <dianecai@gmail.com>
2015-08-14 14:24:38 -0400

    all done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate", 1, "F", "Blonde", "Silly", 1),
+("Rowdy", 2, "F", "Brown", "Rowdy", 0),
+("Tabitha", 5, "F", "Green", "Misanthropic", 0),
+("Sergeant Brown", 3, "F", "White", "Polite", 1),
+("Melissa", 2, "M", "Black", "Earnest", 0),
+("Grinch", 7, "F", "Pink", "Fresh", 1),
+("Wendy", 7, "M", "Grey", "Slap-happy", 0),
+((null), 10, "M", "Blue", "Disoriented", 0);
